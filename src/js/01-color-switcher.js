0af05807735b4a405ab62cc1608933b025ef35c7
Dubyshkina <ek.kalyazina@gmail.com>
2023-03-01 17:43:48 +0200

    did first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let intervalId = null;
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+function changeColor (event) {
+ intervalId = setInterval (() => {
+    body.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+ }, 1000);
+ 
+};
+
+startBtn.addEventListener('click', changeColor);
+
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+});
+    
+    
+
 
