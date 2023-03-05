77b84fe9399b1fa6e9ccba42ecb0fc1aef2fa8ce
Dubyshkina <121130932+Dubyshkina@users.noreply.github.com>
2023-03-05 13:38:20 +0000

    Deploying to gh-pages from @ Dubyshkina/goit-js-hw-09@eab99d971423501e0d288267a5f2dff85649f7f5 🚀

---
+++ 01-color-switcher.39ffe200.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let n=null;t.addEventListener("click",(function(e){t.disabled=!0,n=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(n),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.39ffe200.js.map
